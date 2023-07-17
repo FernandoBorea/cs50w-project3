2643b2e8d98fa95fd4c36991a4170b09eadf0b95
FernandoBorea <fernandoborea1998@gmail.com>
2023-07-16 19:04:22 -0600

    Started email view

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -17,6 +17,7 @@ function compose_email() {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#email-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields
@@ -29,6 +30,7 @@ function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#email-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
@@ -47,7 +49,7 @@ function load_mailbox(mailbox) {
       // Create email anchor element
       email_a_element = document.createElement('a');
       email_a_element.className = !email.read? 'list-group-item list-group-item-action' : 'list-group-item list-group-item-action read';
-      email_a_element.href = '#';
+      // email_a_element.href = '#';
       email_a_element.id = email.id;
 
       // Create email div
@@ -71,6 +73,9 @@ function load_mailbox(mailbox) {
 
       // Build anchor element
       email_a_element.append(email_div_element);
+
+      // Add event listener
+      email_a_element.addEventListener('click', () => show_email(email.id));
 
       // Append to email view
       document.querySelector('#emails-view').append(email_a_element);
@@ -103,4 +108,96 @@ function send_email () {
   // Prevent default behavior
   return false;
 
+}
+
+function show_email(email_id) {
+
+  // Show the email and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#email-view').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  // Get email
+  fetch(`/emails/${email_id}`)
+  .then(response => response.json())
+  .then(email => {
+
+    // Log email
+    console.log(email);
+
+    // Header elements
+    email_header = document.createElement('div');
+    email_header.className = 'card-header';
+
+    sender_div = document.createElement('div');
+    sender_div.className = 'd-flex w-100';
+
+    sender_title = document.createElement('p');
+    sender_title.className = 'font-weight-bold';
+    sender_title.innerHTML = 'From: &nbsp;';
+
+    sender_email = document.createElement('p');
+    sender_email.className = 'card-text';
+    sender_email.innerHTML = email.sender;
+
+    receiver_div = document.createElement('div');
+    receiver_div.className = 'd-flex w-100';
+
+    receiver_title = document.createElement('h6');
+    receiver_title.className = 'font-weight-bold';
+    receiver_title.innerHTML = 'To: &nbsp;';
+
+    receiver_email = document.createElement('p');
+    receiver_email.className = 'card-text';
+    receiver_email.innerHTML = email.recipients;
+
+    subject_div = document.createElement('div');
+    subject_div.className = 'd-flex w-100';
+
+    subject_title = document.createElement('p');
+    subject_title.className = 'mb-3 font-weight-bold';
+    subject_title.innerHTML = 'Subject: &nbsp;';
+
+    subject_content = document.createElement('p');
+    subject_content.className = 'card-text';
+    subject_content.innerHTML = email.subject;
+
+    timestamp = document.createElement('p');
+    timestamp.className = 'card-subtitle text-muted font-weight-bold';
+    timestamp.innerHTML = `On ${email.timestamp}`;
+
+    // Build header
+    sender_div.append(sender_title);
+    sender_div.append(sender_email);
+
+    receiver_div.append(receiver_title);
+    receiver_div.append(receiver_email);
+
+    subject_div.append(subject_title);
+    subject_div.append(subject_content);
+
+    email_header.append(sender_div);
+    email_header.append(receiver_div);
+    email_header.append(subject_div);
+    email_header.append(timestamp);
+
+    // Body elements
+    email_body = document.createElement('div');
+    email_body.className = 'card-body';
+
+    email_content = document.createElement('p');
+    email_content.className = 'card-text';
+    email_content.innerHTML = email.body;
+
+    // Build body
+    email_body.append(email_content)
+
+    // Build email card
+    email_card = document.querySelector('#email-view');
+    email_card.append(email_header);
+    email_card.append(email_body);
+  });
+
+  // Prevent default behavior
+  return false
 }
